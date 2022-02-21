1421118b35663dbf1bfc9648568db67f406920cc
Dreyclin <69818696+Dreyclin@users.noreply.github.com>
2022-02-21 20:24:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+button.onclick = function tabCheck() {
+    var x = document.getElementById('tabButton');
+    if (x.className === '') {
+        x.className += 'active'
+    } else {
+        stop;
+    }
+}
+    
